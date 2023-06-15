0dde0968c8fe50c1f8ce6956f652e0144f73fcbd
snake-4 <18491360+snake-4@users.noreply.github.com>
2023-06-15 22:38:21 +0300

    sndPlaySoundA is no longer an empty func

--- EZUSBToLibusb/WinmmExports.cpp
+++ EZUSBToLibusb/WinmmExports.cpp
@@ -2,9 +2,24 @@
 #define MMNOSOUND
 #include <Windows.h>
 
+using pSndPlaySoundA_t = BOOL(WINAPI*)(LPCSTR, UINT);
+pSndPlaySoundA_t pSndPlaySoundA = nullptr;
+
 extern "C" {
-	__declspec(dllexport) BOOL sndPlaySoundA() {
-		return TRUE;
+	__declspec(dllexport) BOOL sndPlaySoundA(LPCSTR pszSound, UINT fuSound) {
+		if (pSndPlaySoundA == nullptr) {
+			HMODULE winmm = LoadLibrary(TEXT("winmm"));
+			if (winmm != NULL) {
+				FARPROC proc = GetProcAddress(winmm, "sndPlaySoundA");
+				if (proc != NULL) {
+					pSndPlaySoundA = reinterpret_cast<pSndPlaySoundA_t>(proc);
+				}
+			}
+		}
+		if (pSndPlaySoundA != nullptr) {
+			return pSndPlaySoundA(pszSound, fuSound);
+		}
+		return FALSE;
 	}
 }
 #endif
