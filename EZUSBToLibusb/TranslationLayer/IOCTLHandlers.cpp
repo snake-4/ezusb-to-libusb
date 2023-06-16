0c5b482693965e972232a6109cc4111cf8e6a4b1
snake-4 <18491360+snake-4@users.noreply.github.com>
2023-06-17 00:36:48 +0300

    Implemented 4 IOCTL handlers

--- EZUSBToLibusb/TranslationLayer/IOCTLHandlers.cpp
+++ EZUSBToLibusb/TranslationLayer/IOCTLHandlers.cpp
@@ -1,6 +1,7 @@
 #define NOMINMAX
 #include <Windows.h>
 #include <numeric>
+#include <cmath>
 #include <libusb-1.0/libusb.h>
 #include "../Globals.h"
 #include "TranslationLayer.h"
@@ -9,6 +10,52 @@
 
 using namespace EZUSB;
 using namespace TL;
+
+static constexpr uint16_t UppercaseAnchorDownloadChunkSize = 64;
+static constexpr uint16_t LowercaseAnchorDownloadChunkSize = 512;
+
+static ioctl_hdl_return_t InternalAnchorDownload(const void* buffer, size_t bufferLen, uint16_t chunkSize, uint16_t writeOffset)
+{
+	int chunkCount = ((bufferLen + chunkSize - 1) / chunkSize);
+
+	for (int i = 0; i < chunkCount; i++)
+	{
+		uint16_t wLength = chunkSize;
+		if (i == (chunkCount - 1) && (bufferLen % chunkSize) != 0) {
+			wLength = bufferLen % chunkSize;
+		}
+
+		uint8_t bRequest = ANCHOR_LOAD_INTERNAL;
+		uint16_t wValue = (i * chunkSize) + writeOffset;
+
+		const void* bufferAddr = reinterpret_cast<const uint8_t*>(buffer) + i * chunkSize;
+		int transferred = GUSBDev.value().WriteControlTransfer(
+			LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE, bRequest, wValue, 0, bufferAddr, wLength
+		);
+
+		if (transferred != wLength) {
+			return ERROR_GEN_FAILURE;
+		}
+	}
+
+	return ERROR_SUCCESS; //lpBytesReturned is undefined
+}
+
+ioctl_hdl_return_t TL::Hdl_IOCTL_Ezusb_ANCHOR_DOWNLOAD(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
+{
+	//chunk count is rounded towards 0 in the original code
+	size_t truncatedLength = inBufferLen - (inBufferLen % LowercaseAnchorDownloadChunkSize);
+	return InternalAnchorDownload(inBuffer, inBufferLen, LowercaseAnchorDownloadChunkSize, 0);
+}
+
+ioctl_hdl_return_t TL::Hdl_IOCTL_EZUSB_ANCHOR_DOWNLOAD(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
+{
+	if (inBufferLen < sizeof(ANCHOR_DOWNLOAD_CONTROL))
+		return ERROR_INVALID_PARAMETER;
+
+	auto req = reinterpret_cast<PANCHOR_DOWNLOAD_CONTROL>(inBuffer);
+	return InternalAnchorDownload(outBuffer, outBufferLen, UppercaseAnchorDownloadChunkSize, req->Offset);
+}
 
 ioctl_hdl_return_t TL::Hdl_IOCTL_Ezusb_VENDOR_REQUEST(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
 {
@@ -30,7 +77,6 @@ ioctl_hdl_return_t TL::Hdl_IOCTL_Ezusb_VENDOR_REQUEST(LPVOID inBuffer, DWORD inB
 
 		status = GUSBDev.value().WriteControlTransfer(LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE,
 			req->bRequest, req->wValue, req->wIndex, writeBuffer.data(), req->wLength);
-		status = std::max(status, 0); //lpBytesReturned is 0 on these requests
 	}
 
 	if (status > 0)
@@ -61,37 +107,6 @@ ioctl_hdl_return_t TL::Hdl_IOCTL_EZUSB_BULK_WRITE(LPVOID inBuffer, DWORD inBuffe
 
 	int err = GUSBDev.value().WriteBulkTransfer(req->pipeNum, outBuffer, outBufferLen, &bytesWritten);
 	return (err == LIBUSB_SUCCESS ? ERROR_SUCCESS : ERROR_GEN_FAILURE);
-}
-
-ioctl_hdl_return_t TL::Hdl_IOCTL_EZUSB_ANCHOR_DOWNLOAD(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
-{
-	if (inBufferLen < sizeof(ANCHOR_DOWNLOAD_CONTROL))
-		return ERROR_INVALID_PARAMETER;
-
-	static constexpr uint16_t ANCHOR_DOWNLOAD_CHUNK_SIZE = 64;
-	auto req = reinterpret_cast<PANCHOR_DOWNLOAD_CONTROL>(inBuffer);
-
-	int chunkCount = ((outBufferLen + ANCHOR_DOWNLOAD_CHUNK_SIZE - 1) / ANCHOR_DOWNLOAD_CHUNK_SIZE);
-	for (int i = 0; i < chunkCount; i++)
-	{
-		uint16_t wLength = ((i == (chunkCount - 1)) &&
-			(outBufferLen % ANCHOR_DOWNLOAD_CHUNK_SIZE)) ?
-			(outBufferLen % ANCHOR_DOWNLOAD_CHUNK_SIZE) : ANCHOR_DOWNLOAD_CHUNK_SIZE;
-
-		uint8_t bRequest = ANCHOR_LOAD_INTERNAL;
-		uint16_t wValue = (i * ANCHOR_DOWNLOAD_CHUNK_SIZE) + req->Offset;
-
-		const void* bufferAddr = reinterpret_cast<uint8_t*>(outBuffer) + i * ANCHOR_DOWNLOAD_CHUNK_SIZE;
-		int transferred = GUSBDev.value().WriteControlTransfer(
-			LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_RECIPIENT_DEVICE, bRequest, wValue, 0, bufferAddr, wLength
-		);
-
-		if (transferred != wLength) {
-			return ERROR_GEN_FAILURE;
-		}
-	}
-
-	return ERROR_SUCCESS; //lpBytesReturned is undefined
 }
 
 ioctl_hdl_return_t TL::Hdl_IOCTL_EZUSB_VENDOR_OR_CLASS_REQUEST(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
@@ -126,7 +141,7 @@ ioctl_hdl_return_t TL::Hdl_IOCTL_EZUSB_GET_DRIVER_VERSION(LPVOID inBuffer, DWORD
 	if (outBufferLen < sizeof(EZUSB_DRIVER_VERSION))
 		return ERROR_GEN_FAILURE; //This function returns STATUS_UNSUCCESSFUL instead of invalid parameter
 
-	PEZUSB_DRIVER_VERSION version = (PEZUSB_DRIVER_VERSION)outBuffer;
+	auto version = (PEZUSB_DRIVER_VERSION)outBuffer;
 	version->MajorVersion = EZUSB_MAJOR_VERSION;
 	version->MinorVersion = EZUSB_MINOR_VERSION;
 	version->BuildVersion = EZUSB_BUILD_VERSION;
@@ -139,3 +154,40 @@ ioctl_hdl_return_t TL::Hdl_IOCTL_Ezusb_GET_CURRENT_CONFIG(LPVOID inBuffer, DWORD
 	return { ERROR_SUCCESS, static_cast<DWORD>(0) };
 }
 
+ioctl_hdl_return_t TL::Hdl_IOCTL_EZUSB_SET_FEATURE(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
+{
+	if (inBufferLen != sizeof(SET_FEATURE_CONTROL))
+		return { ERROR_INVALID_PARAMETER, static_cast<DWORD>(0) }; //lpBytesReturned is set to 0
+
+	auto req = (PSET_FEATURE_CONTROL)inBuffer;
+
+	GUSBDev.value().WriteControlTransfer(LIBUSB_REQUEST_TYPE_STANDARD | LIBUSB_RECIPIENT_DEVICE,
+		LIBUSB_REQUEST_SET_FEATURE, req->FeatureSelector, req->Index, nullptr, 0
+	);
+
+	return ERROR_GEN_FAILURE; //This function ALWAYS returns STATUS_UNSUCCESSFUL. Maybe a mistake in the EZUSB.sys?
+}
+
+ioctl_hdl_return_t TL::Hdl_IOCTL_Ezusb_GET_STRING_DESCRIPTOR(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
+{
+	if (inBufferLen != sizeof(GET_STRING_DESCRIPTOR_IN) || outBufferLen == 0)
+		return ERROR_INVALID_PARAMETER;
+
+	auto req = (PGET_STRING_DESCRIPTOR_IN)inBuffer;
+	auto buffer = GUSBDev.value().GetStringDescriptor(req->Index, req->LanguageId);
+
+	if (buffer.size() == 0) {
+		return ERROR_GEN_FAILURE;
+	}
+
+	size_t bytesToWrite = std::min(static_cast<size_t>(outBufferLen), buffer.size());
+	std::memcpy(outBuffer, buffer.data(), bytesToWrite);
+
+	return { ERROR_SUCCESS, static_cast<DWORD>(bytesToWrite) };
+}
+
+ioctl_hdl_return_t TL::Hdl_IOCTL_Ezusb_RESET(LPVOID inBuffer, DWORD inBufferLen, LPVOID outBuffer, DWORD outBufferLen)
+{
+	GUSBDev.value().ResetDevice();
+	return ERROR_SUCCESS;
+}
--- EZUSBToLibusb/TranslationLayer/IOCTLHandlers.h
+++ EZUSBToLibusb/TranslationLayer/IOCTLHandlers.h
@@ -3,11 +3,17 @@
 #include "TranslationLayer.h"
 
 namespace TL {
+	//Lowercase functions
+	ioctl_hdl_return_t Hdl_IOCTL_Ezusb_GET_CURRENT_CONFIG(LPVOID, DWORD, LPVOID, DWORD);
+	ioctl_hdl_return_t Hdl_IOCTL_Ezusb_GET_STRING_DESCRIPTOR(LPVOID, DWORD, LPVOID, DWORD);
 	ioctl_hdl_return_t Hdl_IOCTL_Ezusb_VENDOR_REQUEST(LPVOID, DWORD, LPVOID, DWORD);
+	ioctl_hdl_return_t Hdl_IOCTL_Ezusb_ANCHOR_DOWNLOAD(LPVOID, DWORD, LPVOID, DWORD);
+	ioctl_hdl_return_t Hdl_IOCTL_Ezusb_RESET(LPVOID, DWORD, LPVOID, DWORD);
+	//Uppercase functions
+	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_ANCHOR_DOWNLOAD(LPVOID, DWORD, LPVOID, DWORD);
 	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_BULK_READ(LPVOID, DWORD, LPVOID, DWORD);
 	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_BULK_WRITE(LPVOID, DWORD, LPVOID, DWORD);
-	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_ANCHOR_DOWNLOAD(LPVOID, DWORD, LPVOID, DWORD);
 	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_VENDOR_OR_CLASS_REQUEST(LPVOID, DWORD, LPVOID, DWORD);
 	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_GET_DRIVER_VERSION(LPVOID, DWORD, LPVOID, DWORD);
-	ioctl_hdl_return_t Hdl_IOCTL_Ezusb_GET_CURRENT_CONFIG(LPVOID, DWORD, LPVOID, DWORD);
+	ioctl_hdl_return_t Hdl_IOCTL_EZUSB_SET_FEATURE(LPVOID, DWORD, LPVOID, DWORD);
 }
